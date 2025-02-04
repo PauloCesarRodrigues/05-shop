c4a54924d19bfb813c8eb72595de1f9dd8569151
Paulo Cesar Rodrigues <pauloceasrrtv321@gmail.com>
2025-02-04 17:24:45 -0300

    add: procut page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import 'keen-slider/keen-slider.min.css'
 import { stripe } from "@/lib/stripe"
 import type { GetStaticProps } from "next"
 import type Stripe from "stripe"
+import Link from "next/link"
 
 
 interface HomeProps{
@@ -31,13 +32,15 @@ export default function Home({products}: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product =>{
         return(
-          <Product className="keen-slider__slide" key={product.id}>
+          <Link key={product.id} href={`/products/${product.id}`} prefetch={false}>
+          <Product className="keen-slider__slide" >
             <Image src={product.imageUrl} width={520} height={480} alt=""/>
             <footer>
               <strong>{product.name}</strong>
               <span>{product.price}</span>
             </footer>
           </Product>
+          </Link>
         )
       })}
     </HomeContainer>
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,39 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
+import type { GetStaticProps } from "next"
+import Image from "next/image"
 import { useRouter } from "next/router"
+import { useEffect } from "react"
 
 export default function Product(){
   const {query} = useRouter()
 
+  useEffect(()=>{
+
+  },[])
+
   return(
-    <h1>Product: {query.id}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camisa XYZ</h1>
+        <span> R$ 79,90</span>
+
+        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure nostrum provident repellat excepturi consectetur voluptate perferendis quod harum dolor repellendus quas unde qui temporibus commodi quo, dolores voluptatem quam voluptates!</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps = async ()=>{
+
+  return{
+    props: {},
+    revalidate: 60 * 60 * 1 //1hora de cache
+  }
 }
